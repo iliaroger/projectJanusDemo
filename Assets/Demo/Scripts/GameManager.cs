6702838957c5674043700852095e5f8ac1564e00
ilia <iliailia@me.com>
2020-12-30 22:05:15 +0100

    added menu

---
+++ Assets/Demo/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool _menuActive = false;
+    [SerializeField]
+    private GameObject _menuBox;
+
+    void Start()
+    {
+        _menuBox.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // triggers the menu
+            _menuActive = !_menuActive;
+            if (_menuActive)
+            {
+                _menuBox.SetActive(true);
+            }
+            else _menuBox.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
